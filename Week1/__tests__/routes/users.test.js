41e88b62e5f1bf338b486c702682d95e2a97121b
Hugo <h.jongen@hotmail.com>
2024-03-29 14:40:01 +0100

    Refactor user route test

--- Week1/__tests__/routes/users.test.js
+++ Week1/__tests__/routes/users.test.js
@@ -18,11 +18,10 @@ describe('Get Users', () => {
     await db.collection('users').insertOne(expected);
     delete expected._id;
 
-
-    
     const res = await request(app).get('/users');
     expect(res.statusCode).toEqual(200);
     expect(res.body.length).toEqual(1);
+    
     expect(res.body[0]).toEqual(expect.objectContaining(expected));
   });
 });
